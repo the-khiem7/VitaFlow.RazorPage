445bd79bdba46099490a9780c97a0e7f5ac3d17c
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-07-17 09:34:01 +0700

    feat: integrate TailwindCSS v3, remove Bootstrap, and apply new stunning layout for Razor Pages

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./VitaFlow.Web/Pages/**/*.{cshtml,cshtml.cs,html}",
+    "./VitaFlow.Web/Views/**/*.{cshtml,cshtml.cs,html}",
+    "./VitaFlow.Web/Areas/**/*.{cshtml,cshtml.cs,html}",
+    "./VitaFlow.Web/wwwroot/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
